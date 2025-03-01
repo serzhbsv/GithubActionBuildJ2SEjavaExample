913c60cc81af60bb8c5dfd5b9ef1bddec28487d5
Serzh5 <sepzh@rambler.ru>
2025-03-01 22:39:32 +0300

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,33 @@
+public class Server {
+
+    public static final int PORT = 8080;
+    public static LinkedList<ServerSomthing> serverList = new LinkedList<>(); // список всех нитей - экземпляров
+    // сервера, слушающих каждый своего клиента
+    public static Story story; // история переписки
+    
+    /**
+     * @param args
+     * @throws IOException
+     */
+    
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(PORT);
+        story = new Story();
+        System.out.println("Server Started");
+        try {
+            while (true) {
+                // Блокируется до возникновения нового соединения:
+                Socket socket = server.accept();
+                try {
+                    serverList.add(new ServerSomthing(socket)); // добавить новое соединенние в список
+                } catch (IOException e) {
+                    // Если завершится неудачей, закрывается сокет,
+                    // в противном случае, нить закроет его:
+                    socket.close();
+                }
+            }
+        } finally {
+            server.close();
+        }
+    }
+}
